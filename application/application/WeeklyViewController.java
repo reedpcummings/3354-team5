31117cdf8d47a3a6f57cc0e86cd8559df0724d33
Reed Cummings <rpc150030@utdallas.edu>
2017-11-03 12:21:35 -0500

    Add files via upload
    more updates

---
+++ application/application/WeeklyViewController.java
@@ -0,0 +1,39 @@
+
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+
+public class WeeklyViewController {
+
+    @FXML
+    void goToAddEventView(ActionEvent event) {
+
+    	try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("addEvent.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("Add Event");
+            stage.setScene(new Scene(root1));  
+            stage.show();
+          }
+		catch(Exception e){
+			e.printStackTrace();
+		}
+    	
+    	
+    }
+
+}
