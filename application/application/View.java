8545639eac9a51031b7f8910f006bec61a707e0d
AlexLundinEducational <aml140830@utdallas.edu>
2017-11-06 09:03:16 -0600

    <View Class>

---
+++ application/application/View.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+
+// Calendar Application
+// Alex Lundin
+// 11-06-2017
+// AML140830  
+// SE 3354.004
+// Software Engineering
+
+public class View {
+
+   // attributes
+   private View currentView;
+
+
+   // constructors
+   
+   // getters
+   
+   private void getView(){
+   }
+   // setters
+   
+   private void setView(){
+   }
+   
+   // methods
+   
+   private String displayTitle(day Day){
+      String titleString = "null";
+      return titleString;
+   }
+
+}
